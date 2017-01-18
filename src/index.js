ebae4b057da2fec54b771f122c8e9ac0595d6be5
liveu <maleficush@gmail.com>
2017-01-18 17:20:08 +0900

    [gulp index.js 추가]

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-/**
- * Created by liveu on 2017-01-18.
- */
+/* @flow*/
+import express from 'express';
+import bodyParser from 'body-parser';
+import multer from 'multer';
+import morgan from 'morgan';
+
+
+const app = express();
+const upload = multer();
+
+//static 서버 구성
+app.use( express.static( __dirname + '/../public' ) );
+
+app.use( morgan( 'dev' ) );
+
+app.use( bodyParser.json() );
+
+app.use( bodyParser.urlencoded( { extended: true } ) );
+
+
+
+app.listen( 7080, () => {
+   console.log('server is listening on port 7080');
+});
+
+// sample routes
+app.get('/hello', ( req, res ) => {
+   res.send('world');
+});
+
+app.get('/add/:x/:y', ( req, res ) => {
+   const x = req.params.x * 1;
+   const y = req.params.y * 1;
+
+   res.send( { sum: x + y });
+});
