4e882ab116ab11932454324cce8c532505ffa57e
liveu <maleficush@gmail.com>
2017-01-18 16:35:14 +0900

    [gulp task 추가]

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,26 +14,44 @@ gulp.task('default', cb =>{
     run( 'server', 'build', 'watch', cb );
 });
 
+
 gulp.task('build', cb => {
     run( 'clean', 'flow', 'babel', 'restart', cb );
 });
 
-// clean build folder -> app
+// [clean] : clean build folder -> app
 gulp.task('clean', cb => {
     rimraf( paths.destination, cb );
 });
 
-//excute unix, linux command
+// [flow] : excute unix, linux command
 gulp.task('flow', shell.task([
     'flow'
 ], { ignoreErrors: true }));
 
+// [babel] : babel compiler로 app폴더로 typeScript compile 진행
 gulp.task('babel', shell.task([
-    'babel src '
+    'babel src --out-dir app'
 ]));
 
 
+
 let express;
-gulp.task('server', () =>{
+
+// [server] : babel로 컴파일된 ts기반의 express서버 구동
+gulp.task('server', () => {
     express = server.new( paths.destination );
 });
+
+gulp.task('restart', () => {
+    express.start.bind( express )();
+});
+
+// [watch] : src내부의 소스 변경시 다시 build를 해준다.
+gulp.task('watch', () => {
+    return watch( paths.js, () => {
+        gulp.start( 'build' );
+    });
+});
+
+
